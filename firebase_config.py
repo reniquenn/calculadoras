4b3b627f874a05be8d9358cd877bde22b654e963
reniquenn <daniel.quintana2025@hotmail.com>
2025-12-25 18:25:26 -0300

    a
    a

--- firebase_config.py
+++ firebase_config.py
@@ -1,12 +1,12 @@
-import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
-import json
+import streamlit as st
 
-# Inicialización segura para Streamlit Cloud
 if not firebase_admin._apps:
-    firebase_key = json.loads(st.secrets["FIREBASE_KEY"])
+    firebase_key = st.secrets["FIREBASE_KEY"]
+
     cred = credentials.Certificate(firebase_key)
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+
